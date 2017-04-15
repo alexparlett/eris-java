8221e03793e6cae059d8cbff9fb1e6d58daaabae
Alex Parlett <alexparlett@gmail.com>
2017-04-15 10:26:17 +0100

    #10: Added attribute accessor

--- eris-graphics/src/main/java/org/homonoia/eris/graphics/drawables/ShaderProgram.java
+++ eris-graphics/src/main/java/org/homonoia/eris/graphics/drawables/ShaderProgram.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import org.homonoia.eris.core.Context;
+import org.homonoia.eris.graphics.drawables.sp.Attribute;
 import org.homonoia.eris.graphics.drawables.sp.Uniform;
 import org.homonoia.eris.resources.GPUResource;
 import org.homonoia.eris.resources.Resource;
@@ -30,18 +31,55 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
+import static java.lang.System.lineSeparator;
+import static java.nio.file.Files.newInputStream;
+import static java.nio.file.Paths.get;
+import static java.util.Objects.nonNull;
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
+import static org.homonoia.eris.graphics.drawables.sp.Uniform.builder;
+import static org.homonoia.eris.resources.Resource.AsyncState.GPU_READY;
+import static org.homonoia.eris.resources.Resource.AsyncState.SUCCESS;
+import static org.lwjgl.opengl.GL20.GL_ACTIVE_ATTRIBUTES;
+import static org.lwjgl.opengl.GL20.GL_ACTIVE_UNIFORMS;
+import static org.lwjgl.opengl.GL20.GL_COMPILE_STATUS;
+import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
+import static org.lwjgl.opengl.GL20.GL_LINK_STATUS;
+import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
+import static org.lwjgl.opengl.GL20.glAttachShader;
+import static org.lwjgl.opengl.GL20.glCompileShader;
+import static org.lwjgl.opengl.GL20.glCreateProgram;
+import static org.lwjgl.opengl.GL20.glCreateShader;
+import static org.lwjgl.opengl.GL20.glDeleteProgram;
+import static org.lwjgl.opengl.GL20.glDeleteShader;
+import static org.lwjgl.opengl.GL20.glGetActiveAttrib;
 import static org.lwjgl.opengl.GL20.glGetActiveUniform;
+import static org.lwjgl.opengl.GL20.glGetAttribLocation;
+import static org.lwjgl.opengl.GL20.glGetProgramInfoLog;
+import static org.lwjgl.opengl.GL20.glGetProgrami;
+import static org.lwjgl.opengl.GL20.glGetProgramiv;
+import static org.lwjgl.opengl.GL20.glGetShaderInfoLog;
+import static org.lwjgl.opengl.GL20.glGetShaderiv;
 import static org.lwjgl.opengl.GL20.glGetUniformLocation;
+import static org.lwjgl.opengl.GL20.glLinkProgram;
+import static org.lwjgl.opengl.GL20.glShaderSource;
+import static org.lwjgl.opengl.GL20.glUseProgram;
 import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
- * Created by alexparlett on 16/04/2016.
+ * Copyright (c) 2015-2017 Homonoia Studios.
+ *
+ * @author alexparlett
+ * @since 15/04/2017
  */
 public class ShaderProgram extends Resource implements GPUResource {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ShaderProgram.class);
+    private static final Logger LOG = getLogger(ShaderProgram.class);
     private int handle;
     private Map<String, Uniform> uniforms = new HashMap<>();
+    private Map<String, Attribute> attributes = new HashMap<>();
     private StringBuffer vertBuffer;
     private StringBuffer fragBuffer;
 
@@ -60,17 +98,17 @@ public class ShaderProgram extends Resource implements GPUResource {
                 .map(JsonElement::getAsJsonObject)
                 .orElseThrow(() -> new IOException("No root found"));
 
-        String frag = Optional.ofNullable(root.getAsJsonPrimitive("frag"))
+        String frag = ofNullable(root.getAsJsonPrimitive("frag"))
                 .map(JsonPrimitive::getAsString)
                 .orElseThrow(() -> new IOException("No 'frag' element found"));
 
-        String vert = Optional.ofNullable(root.getAsJsonPrimitive("vert"))
+        String vert = ofNullable(root.getAsJsonPrimitive("vert"))
                 .map(JsonPrimitive::getAsString)
                 .orElseThrow(() -> new IOException("No 'vert' element found"));
 
         ShaderPreprocessor shaderPreprocessor = new ShaderPreprocessor();
 
-        try (InputStream fragStream = resourceCache.get(Stream.class, Paths.get(frag))
+        try (InputStream fragStream = resourceCache.get(Stream.class, get(frag))
                 .map(Stream::asInputStream)
                 .orElseThrow(() -> new IOException(frag + " not found."))) {
 
@@ -79,30 +117,33 @@ public class ShaderProgram extends Resource implements GPUResource {
 
         shaderPreprocessor.reset();
 
-        try (InputStream vertStream = resourceCache.get(Stream.class, Paths.get(vert))
+        try (InputStream vertStream = resourceCache.get(Stream.class, get(vert))
                 .map(Stream::asInputStream)
                 .orElseThrow(() -> new IOException(vert + " not found."))) {
 
             vertBuffer = shaderPreprocessor.process(vertStream, vert);
         }
 
-        setState(AsyncState.GPU_READY);
+        setState(GPU_READY);
     }
 
     @Override
     public void use() {
-        GL20.glUseProgram(handle);
+        glUseProgram(handle);
 
         uniforms.values()
                 .stream()
-                .filter(uniform -> Objects.nonNull(uniform.getData()))
+                .filter(uniform -> nonNull(uniform.getData()))
                 .forEach(Uniform::bindUniform);
     }
 
     public Optional<Uniform> getUniform(String uniform) {
-        return Optional.ofNullable(uniforms.get(uniform));
+        return ofNullable(uniforms.get(uniform));
     }
 
+    public Optional<Attribute> getAttribute(String name) {
+        return ofNullable(attributes.get(name));
+    }
 
     @Override
     public int getHandle() {
@@ -111,86 +152,101 @@ public class ShaderProgram extends Resource implements GPUResource {
 
     @Override
     public void reset() {
-        if (handle != MemoryUtil.NULL) {
-            GL20.glDeleteProgram(handle);
+        if (handle != NULL) {
+            glDeleteProgram(handle);
             handle = 0;
         }
     }
 
     @Override
     public void compile() throws IOException {
-        Objects.requireNonNull(fragBuffer);
-        Objects.requireNonNull(vertBuffer);
+        requireNonNull(fragBuffer);
+        requireNonNull(vertBuffer);
 
         try (MemoryStack stack = stackPush()) {
-            int vertex = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
-            GL20.glShaderSource(vertex, vertBuffer.toString());
-            GL20.glCompileShader(vertex);
+            int vertex = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertex, vertBuffer.toString());
+            glCompileShader(vertex);
 
 
             IntBuffer success = stack.callocInt(1);
-            GL20.glGetShaderiv(vertex, GL20.GL_COMPILE_STATUS, success);
+            glGetShaderiv(vertex, GL_COMPILE_STATUS, success);
             if (success.get() < 1) {
-                String shaderInfoLog = GL20.glGetShaderInfoLog(vertex);
+                String shaderInfoLog = glGetShaderInfoLog(vertex);
 
-                GL20.glDeleteShader(vertex);
+                glDeleteShader(vertex);
                 throw new IOException("Vertex Shader compile error\n" + shaderInfoLog);
             }
 
-            int fragment = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
-            GL20.glShaderSource(fragment, fragBuffer.toString());
-            GL20.glCompileShader(fragment);
+            int fragment = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragment, fragBuffer.toString());
+            glCompileShader(fragment);
 
             success.clear();
-            GL20.glGetShaderiv(fragment, GL20.GL_COMPILE_STATUS, success);
+            glGetShaderiv(fragment, GL_COMPILE_STATUS, success);
             if (success.get() < 1) {
-                String shaderInfoLog = GL20.glGetShaderInfoLog(fragment);
+                String shaderInfoLog = glGetShaderInfoLog(fragment);
 
-                GL20.glDeleteShader(fragment);
+                glDeleteShader(fragment);
                 throw new IOException("Fragment Shader compile error\n" + shaderInfoLog);
             }
 
-            handle = GL20.glCreateProgram();
+            handle = glCreateProgram();
 
-            GL20.glAttachShader(handle, vertex);
-            GL20.glAttachShader(handle, fragment);
-            GL20.glLinkProgram(handle);
+            glAttachShader(handle, vertex);
+            glAttachShader(handle, fragment);
+            glLinkProgram(handle);
 
             success.clear();
-            GL20.glGetProgramiv(handle, GL20.GL_LINK_STATUS, success);
+            glGetProgramiv(handle, GL_LINK_STATUS, success);
             if (success.get() < 1) {
-                String programInfoLog = GL20.glGetProgramInfoLog(handle);
+                String programInfoLog = glGetProgramInfoLog(handle);
 
-                GL20.glDeleteShader(vertex);
-                GL20.glDeleteShader(fragment);
-                GL20.glDeleteProgram(handle);
+                glDeleteShader(vertex);
+                glDeleteShader(fragment);
+                glDeleteProgram(handle);
                 handle = 0;
 
-                throw new IOException("Fragment Shader link error\n" + programInfoLog);
+                throw new IOException("Shader link error\n" + programInfoLog);
             }
 
-            GL20.glDeleteShader(vertex);
-            GL20.glDeleteShader(fragment);
+            glDeleteShader(vertex);
+            glDeleteShader(fragment);
 
             IntBuffer type = stack.callocInt(1);
             IntBuffer size = stack.callocInt(1);
-            for (int i = 0; i < GL20.glGetProgrami(handle, GL20.GL_ACTIVE_UNIFORMS); i++) {
+            for (int i = 0; i < glGetProgrami(handle, GL_ACTIVE_UNIFORMS); i++) {
                 type.clear();
                 size.clear();
 
                 String name = glGetActiveUniform(handle, i, size, type);
                 int location = glGetUniformLocation(handle, name);
 
-                Uniform uniform = Uniform.builder()
+                Uniform uniform = builder()
                         .type(type.get())
                         .location(location)
                         .build();
 
                 uniforms.put(name, uniform);
             }
+
+            for (int i = 0; i < glGetProgrami(handle, GL_ACTIVE_ATTRIBUTES); i++) {
+                type.clear();
+                size.clear();
+
+                String name = glGetActiveAttrib(handle, i, size, type);
+                int location = glGetAttribLocation(handle, name);
+
+                Attribute attribute = Attribute.builder()
+                        .type(type.get())
+                        .location(location)
+                        .build();
+
+                attributes.put(name, attribute);
+            }
         }
 
-        setState(AsyncState.SUCCESS);
+        setState(SUCCESS);
     }
 
     public final static class ShaderPreprocessor {
@@ -218,12 +274,12 @@ public class ShaderProgram extends Resource implements GPUResource {
                         continue;
                     }
 
-                    try (InputStream includeStream = Files.newInputStream(Paths.get(filename))) {
+                    try (InputStream includeStream = newInputStream(get(filename))) {
                         out.append(process(includeStream, filename));
                     }
                 } else {
                     out.append(line);
-                    out.append(System.lineSeparator());
+                    out.append(lineSeparator());
                 }
             }
 
---
+++ eris-graphics/src/main/java/org/homonoia/eris/graphics/drawables/sp/Attribute.java
@@ -0,0 +1,17 @@
+package org.homonoia.eris.graphics.drawables.sp;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Copyright (c) 2015-2017 Homonoia Studios.
+ *
+ * @author alexparlett
+ * @since 15/04/2017
+ */
+@Getter
+@Builder
+public class Attribute {
+    private int type;
+    private int location;
+}
